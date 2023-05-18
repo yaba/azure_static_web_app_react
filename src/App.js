afece798c871b60bc3e14b23179c9d89ff82b0e2
YaBa <polido@gmail.com>
2023-05-18 11:46:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} - Env Var: %REACT_APP_MSG%</div>;
+  return <div>REACT_APP_MSG: {process.env.REACT_APP_MSG ?? "No environment variables exposed to the browser"}</div>;
+  
 }
 
 export default App;
