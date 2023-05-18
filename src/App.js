8f11f46bb3804cfb0c205dd7a67393bcaa8ba0b4
YaBa <polido@gmail.com>
2023-05-18 11:00:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   const myappsetting = process.env.TEST_MSG;
-  return <div>Hello {value}</div></br><p>Env Var: {myappsetting}</p>;
+  return <div>Hello {value}</div><br /><p>Env Var: {myappsetting}</p>;
 }
 
 export default App;
